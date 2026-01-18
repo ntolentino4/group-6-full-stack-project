e445251141ec67e96bb76056e14324dbd4f41d51
unknown <arohleder@academic.rrc.ca>
2026-01-18 17:19:12 -0600

    Add: T.4 requirements header and footer components. No Axe DevTool warnings

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Header from './components/header/Header'
 import ExpenseFilter from './components/expense-filter/ExpenseFilter'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <ExpenseFilter />
+    <>
+      <Header />
+      <ExpenseFilter />
+      <Footer />
+    </>
   )
 }
 
