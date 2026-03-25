1d38552e89daa02a478f95341d7ea36343c1080f
Neil Tolentino <ntolentino4@academic.rrc.ca>
2026-03-24 23:23:55 -0500

    feat: Defined Prisma Schema model to create table for database.

--- apps/backend/prisma/client.ts
+++ apps/backend/prisma/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import process from "process";
