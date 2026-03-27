c114d713e19d9c3aa8d69a23d09945d811d00948
Neil Tolentino <ntolentino4@rrc.ca>
2026-03-27 16:14:43 -0500

    Update client.ts
    Updated Client.ts

--- apps/backend/prisma/client.ts
+++ apps/backend/prisma/client.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from "../src/generated/prisma/client/client";
 import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
