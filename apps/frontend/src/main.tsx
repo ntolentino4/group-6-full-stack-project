9a9f8d5f6aec8da0a5c00ee97885f722ded0c810
hgcastillo <hcastillo3@academic.rrc.ca>
2026-04-19 21:53:15 -0500

    Completed the T.1: Clerk Auth Setup requirements

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+// Import your Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </ClerkProvider>
+  </React.StrictMode>,
+);
